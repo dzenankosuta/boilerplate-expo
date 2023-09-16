e1a1c894dadebbcede3b19cf2b5e118ac1159e1b
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2023-09-16 15:02:38 +0200

    expo => 49

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,17 @@
 // eslint-disable-next-line
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig } = require("@expo/metro-config");
 
 // eslint-disable-next-line
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
+    // eslint-disable-next-line
+  } = await getDefaultConfig(__dirname);
   return {
     transformer: {
       // eslint-disable-next-line
       babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      assetPlugins: ["expo-asset/tools/hashAssetFiles"],
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== "svg"),
